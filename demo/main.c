1c6c7d7c2a15e1e01b73100aef280f14aee5ea05
acctonyolandaliu <48586051+acctonyolandaliu@users.noreply.github.com>
2019-04-15 23:12:28 +0800

    Update main.c  2019/4/15 23:11

--- demo/main.c
+++ demo/main.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-	printf("(on 10.100.41.12) Pull source code & Build image. 2019/4/15 23:03\n");
+	printf("(on 10.100.41.12) Pull source code & Build image. 2019/4/15 23:11\n");
 	getchar();
 
 	return 0;
