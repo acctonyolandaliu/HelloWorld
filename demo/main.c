e194e43f4334a5dd491a9d08d8ca41a53b23e919
acctonyolandaliu <48586051+acctonyolandaliu@users.noreply.github.com>
2019-09-17 11:06:29 +0800

    Update main.c 2019/9/17 11:06
    2019/9/17 11:06

--- demo/main.c
+++ demo/main.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-	printf("(on Jenkins slave node 10.100.41.12) Pull source code & Build image. 2019/9/16 16:48\n");
+	printf("(on Jenkins slave node 10.100.41.12) Pull source code & Build image. 2019/9/17 11:06\n");
 	getchar();
 
 	return 0;
